fc07822ecaf419dc6042fcccf559f9a55e92f0d1
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-06-12 22:04:23 +0500

    Patient entity

--- src/main/java/com/example/HospitalManagementSystem/entity/Patient.java
+++ src/main/java/com/example/HospitalManagementSystem/entity/Patient.java
@@ -1,25 +1,39 @@
 package com.example.HospitalManagementSystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.example.HospitalManagementSystem.entity.types.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 30)
+    private String name;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private LocalDate birthDate;
+
+    private String gender;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
 }

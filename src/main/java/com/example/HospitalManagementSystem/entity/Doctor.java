4f25cb4205ec78c579ef6efc36979000be5db655
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-06-16 19:54:53 +0500

    doctor entity

---
+++ src/main/java/com/example/HospitalManagementSystem/entity/Doctor.java
@@ -0,0 +1,38 @@
+package com.example.HospitalManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 30)
+    private String specialization;
+
+    @ManyToMany(mappedBy = "doctors")
+    private Set<Department> departments = new HashSet<>();
+
+    @OneToMany(mappedBy = "doctor")
+    private List<Appointment> appointments = new ArrayList<>();
+}

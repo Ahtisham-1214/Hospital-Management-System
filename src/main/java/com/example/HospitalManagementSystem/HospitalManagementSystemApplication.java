1e04ddb8d887507782a5f328a44468bfd67b401a
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-06-17 19:03:43 +0500

    added web file

--- src/main/java/com/example/HospitalManagementSystem/HospitalManagementSystemApplication.java
+++ src/main/java/com/example/HospitalManagementSystem/HospitalManagementSystemApplication.java
@@ -2,12 +2,19 @@ package com.example.HospitalManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home(){
+        return "redirect:/index.html";
+    }
 }

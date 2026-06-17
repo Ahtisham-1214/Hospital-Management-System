211b1a6886c40d2030e8f59dd6e78eb5e016fec6
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2026-06-17 19:01:28 +0500

    updated patient entity

--- src/main/java/com/example/HospitalManagementSystem/entity/Patient.java
+++ src/main/java/com/example/HospitalManagementSystem/entity/Patient.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -36,4 +38,12 @@ public class Patient {
 
     @Enumerated(EnumType.STRING)
     private BloodGroupType bloodGroup;
+
+    @OneToOne(cascade = {CascadeType.ALL}, orphanRemoval = true)
+    @JoinColumn(name = "patient_insurance_id") // owning side
+    private Insurance insurance;
+
+    @OneToMany(mappedBy = "patient", cascade = {CascadeType.REMOVE}, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<Appointment> appointments = new ArrayList<>();
+
 }
